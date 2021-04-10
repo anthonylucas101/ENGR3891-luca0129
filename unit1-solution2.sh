310b8c8f313cb21fd01d653058375847b89f5d97
Anthony <getupadel@gmail.com>
2021-04-10 14:42:24 +0930

    commit ex2

---
+++ unit1-solution2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir -m 775 unit1exercise2
+cd unit1exercise2
+sudo install -d verklettt/aufwarfheit/enwarfst -m 337 -o games -g tape
+sudo install -d angetritter/beschmeckung/ausgesprachs -m 323 -o news -g student
+sudo install -d angetritter/beschmeckung/aufkletts -m 420 -o proxy -g floppy
+sudo install -d verklettt/aufwarfheit/bestehs -m 341 -o news -g fax
+sudo install -d enpflumen/aufgesitzse/zerraucher -m 227 -o nobody -g tape
+sudo install -d angetritter/antrittung/ankaeser -m 444 -o games -g dip
+sudo install -d angetritter/beschmeckung/ausrauchheit -m 721 -o nobody -g cdrom
+sudo install -d enpflumen/beklettte -m 243 -o mail -g student
+sudo install -d enpflumen/aufgesitzse -m 173 -o news -g cdrom
+sudo install -d angetritter/antrittung -m 116 -o mail -g student
+sudo install -d verklettt/aufwarfheit -m 725 -o proxy -g voice
+sudo install -d angetritter/beschmeckung -m 034 -o lp -g audio
+sudo install -d verrennkeit -m 603 -g floppy -o news
+sudo install -d enpflumen -m 012 -g news -o news
+sudo install -d angetritter -m 507 -g tape -o news
+sudo install -d besprachtest -m 353 -g fax -o proxy
+sudo install -d enpflums -m 073 -g fax -o news
+sudo install -d verklettt -m 711 -g tape -o proxy
+sudo install -d aufgewarfs -m 653 -g floppy -o mail
+sudo install -d aushundheit -m 610 -g student -o news
