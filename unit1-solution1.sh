cfea74db1a6e5823e35f4405b574a33f4d2cd924
Anthony <getupadel@gmail.com>
2021-04-10 14:25:17 +0930

    New commit

---
+++ unit1-solution1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+mkdir -m 775 unit1exercise1
+cd unit1exercise1
+mkdir -p -m 367 zertritter/aufklettheit
+mkdir -p -m 231 auswitzer/angetraust/auspflumse
+mkdir -p -m 212 zertritter/aufklettheit/angerabarbheit
+mkdir -p -m 205 verkaesen/aufwarftest/aufgerennung
+mkdir -p -m 162 auswitzer/aufgerabarbt/einkrauen
+mkdir -p -m 336 auswitzer/aufgerabarbt/befahrt
+mkdir -p -m 572 zertritter/aufklettheit/angekrauung
+mkdir -p -m 2 auswitzer/angewarfen/aufgesetzheit
+mkdir -m 660 enhundst
+mkdir -m 3 bepflumheit
+mkdir -m 72 zersinnse
+mkdir -m 316 besprachst
+mkdir -m 400 verrabarbst
+chmod 626 zertritter
+cd auswitzer
+chmod 762 aufgerabarbt
+chmod 2 angewarfen
+chmod 446 angetraust
+cd ../
+chmod 500 auswitzer
+chmod 507 verkaesen/aufwarftest
+chmod 774 verkaesen
