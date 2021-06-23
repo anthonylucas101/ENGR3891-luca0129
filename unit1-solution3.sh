3f7e671f40ccd85e74844efcd5c704424bf305e5
anthonylucas101 <64211327+anthonylucas101@users.noreply.github.com>
2021-06-23 21:30:16 +0930

    Add files via upload
    final

---
+++ unit1-solution3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+v="install -d -m"
+cd unit1exercise3
+$v 764 -oproxy -gdip anwarftest
+$v 4650 -ouucp -gproxy einrauchen
+$v 4354 -oproxy -gmail angetrittst
+$v 405 -ostudent -gdip einrabarber
+$v 230 -onews -gmail aufgetritttete
+$v 4071 -omail -gaudio auswitzte
+$v 460 -ouucp -gdip zerrabarbt
+$v 4337 -ouucp -gdip zersetzt
+$v 4016 -ogames -gvoice aufgetritttete/aufgefahrtete
+$v 432 -omail -gmail zerrabarbt/angerabarbs
+$v 4245 -omail -guucp zersetzt/ausgekrauen
+$v 4727 -omail -gproxy angetrittst/versprachst
+$v 641 -ostudent -gnews auswitzte/angeschmecktest
+$v 4723 -ogames -gstudent zerrabarbt/angerabarbs/bestehkeit
+$v 4274 -onobody -gfloppy zerrabarbt/angerabarbs/bekraus
+$v 251 -onews -gdip aufgetritttete/aufgefahrtete/aufgewitzung
+$v 620 -omail -gfax angetrittst/versprachst/gepflumse
+$v 4024 -oproxy -gnews aufgetritttete/aufgefahrtete/ausgehaltheit
+$v 4756 -onews -guucp aufgetritttete/aufgefahrtete/aufrennt
+$v 732 -omail -gstudent zerrabarbt/angerabarbs/anfahren
---
+++ unit1-solution4.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+v="install -d -m"
+s="-ostudent"
+cd unit1exercise4
+$v 2736 $s -gproxy einwitzse/verklettung/belaufung
+$v 603 $s -gfloppy einwitzse/befahrtete/angesetztest
+$v 2542 $s -gvoice verfahrtest/angegeher/angehalttete
+$v 2376 $s -gmail auffahren/gekaest/zerstehung
+$v 2135 $s -gtape auffahren/gekaest/auftrittung
+$v 2102 $s -gfloppy auffahren/gekaest/enrauchkeit
+$v 707 $s -gdip einwitzse/befahrtete/aufstehen
+$v 457 $s -gproxy aufwitzung
+$v 206 $s -gtape einstehst
+$v 2650 $s -gstudent gelaufkeit
+$v 2127 $s -gaudio verfahrtest
+$v 147 $s -gdip verfahrs
+$v 320 $s -gfax begehs
+$v 257 $s -gtape einwitzse/befahrtete
+$v 047 $s -gcdrom einwitzse/verklettung
+$v 2523 $s -gvoice einwitzse
+$v 230 $s -gstudent auffahren/gekaest
+$v 2111 $s -gnews auffahren
+$v 2657 $s -gvoice verfahrs/bewitzse
+$v 2761 $s -gcdrom verfahrtest/angegeher
