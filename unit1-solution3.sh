d6491a08b85d1ff0ed4aff0adce9a1c30539d71f
anthonylucas101 <64211327+anthonylucas101@users.noreply.github.com>
2021-07-20 19:29:37 +0930

    sudo/mkdir

--- unit1-solution3.sh
+++ unit1-solution3.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 v="install -d -m"
 cd unit1exercise3
-$v 764 -oproxy -gdip anwarftest
-$v 4650 -ouucp -gproxy einrauchen
-$v 4354 -oproxy -gmail angetrittst
-$v 405 -ostudent -gdip einrabarber
-$v 230 -onews -gmail aufgetritttete
-$v 4071 -omail -gaudio auswitzte
-$v 460 -ouucp -gdip zerrabarbt
-$v 4337 -ouucp -gdip zersetzt
-$v 4016 -ogames -gvoice aufgetritttete/aufgefahrtete
-$v 432 -omail -gmail zerrabarbt/angerabarbs
-$v 4245 -omail -guucp zersetzt/ausgekrauen
-$v 4727 -omail -gproxy angetrittst/versprachst
-$v 641 -ostudent -gnews auswitzte/angeschmecktest
-$v 4723 -ogames -gstudent zerrabarbt/angerabarbs/bestehkeit
-$v 4274 -onobody -gfloppy zerrabarbt/angerabarbs/bekraus
-$v 251 -onews -gdip aufgetritttete/aufgefahrtete/aufgewitzung
-$v 620 -omail -gfax angetrittst/versprachst/gepflumse
-$v 4024 -oproxy -gnews aufgetritttete/aufgefahrtete/ausgehaltheit
-$v 4756 -onews -guucp aufgetritttete/aufgefahrtete/aufrennt
-$v 732 -omail -gstudent zerrabarbt/angerabarbs/anfahren
+mkdir unit1exercise3
+sudo $v 764 -oproxy -gdip anwarftest
+sudo $v 4650 -ouucp -gproxy einrauchen
+sudo $v 4354 -oproxy -gmail angetrittst
+sudo $v 405 -ostudent -gdip einrabarber
+sudo $v 230 -onews -gmail aufgetritttete
+sudo $v 4071 -omail -gaudio auswitzte
+sudo $v 460 -ouucp -gdip zerrabarbt
+sudo $v 4337 -ouucp -gdip zersetzt
+sudo $v 4016 -ogames -gvoice aufgetritttete/aufgefahrtete
+sudo $v 432 -omail -gmail zerrabarbt/angerabarbs
+sudo $v 4245 -omail -guucp zersetzt/ausgekrauen
+sudo $v 4727 -omail -gproxy angetrittst/versprachst
+sudo $v 641 -ostudent -gnews auswitzte/angeschmecktest
+sudo $v 4723 -ogames -gstudent zerrabarbt/angerabarbs/bestehkeit
+sudo $v 4274 -onobody -gfloppy zerrabarbt/angerabarbs/bekraus
+sudo $v 251 -onews -gdip aufgetritttete/aufgefahrtete/aufgewitzung
+sudo $v 620 -omail -gfax angetrittst/versprachst/gepflumse
+sudo $v 4024 -oproxy -gnews aufgetritttete/aufgefahrtete/ausgehaltheit
+sudo $v 4756 -onews -guucp aufgetritttete/aufgefahrtete/aufrennt
+sudo $v 732 -omail -gstudent zerrabarbt/angerabarbs/anfahren
